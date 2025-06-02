40cfd0834c656d19dd6e38e54298dfdc7334d3dd
jluis-imac <skateluis27@hotmail.com>
2025-06-02 12:02:58 -0600

    fix: remove fnValidaEstructuraCurp blink validation

--- src/utils/blinkid.js
+++ src/utils/blinkid.js
@@ -758,31 +758,6 @@ function fnEliminaCaracteresEspeciales(texto) {
     .normalize()
 }
 
-function fnValidaEstructuraCurp(curp) {
-  var re =
-      /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/,
-    validado = curp.match(re)
-
-  if (!validado)
-    //Coincide con el formato general?
-    return '?'
-
-  //Validar que coincida el digito verificador
-  function digitoVerificador(curp17) {
-    var diccionario = '0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ',
-      lngSuma = 0.0,
-      lngDigito = 0.0
-    for (var i = 0; i < 17; i++)
-      lngSuma = lngSuma + diccionario.indexOf(curp17.charAt(i)) * (18 - i)
-    lngDigito = 10 - (lngSuma % 10)
-    if (lngDigito === 10) return '?'
-    return lngDigito
-  }
-  if (validado[2] !== digitoVerificador(validado[1])) return '?'
-
-  return curp //Validado
-}
-
 function fnValidaNoVacios(cadena) {
   let ret
   if (cadena === '' || cadena == null || cadena === 'Undefinied') {
@@ -893,7 +868,7 @@ function parseaOcr(objetoData) {
   ) {
     curpMB = '?'
   } else {
-    curpMB = fnValidaEstructuraCurp(objetoData.frontViz.personalIdNumber.latin)
+    curpMB = objetoData.frontViz.personalIdNumber.latin
   }
 
   let estadoNacimientoMB = getEstRenapo(curpMB)
