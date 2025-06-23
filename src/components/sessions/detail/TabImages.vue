effa59412e88ebc61f63e75d22c9e770fa7bd74f
jluis-imac <jluiscastillo1986@gmail.com>
2025-06-23 17:50:59 -0600

    feat: add lightbox images

--- src/components/sessions/detail/TabImages.vue
+++ src/components/sessions/detail/TabImages.vue
@@ -1,35 +1,69 @@
 <template>
   <div>
     <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Imagenes</h3>
-    <div class="flex flex-row gap-6 items-top">
-      <fwb-img
+    <div
+      v-if="imgs.length"
+      class="flex flex-row gap-6 items-top"
+    >
+      <Image
         v-if="data?.imagenFrontal"
-        class="rounded-xl"
-        alt="flowbite-vue"
         size="w-64"
         :src="`data:image/jpeg;base64,${data?.imagenFrontal}`"
+        @click="showLightbox(0)"
       />
-      <fwb-img
+      <Image
         v-if="data?.imagenReverso"
-        class="rounded-xl"
-        alt="flowbite-vue"
+        class="cursor-pointer rounded-xl transition-transform duration-300 ease-in-out hover:scale-105"
         size="w-64"
         :src="`data:image/jpeg;base64,${data?.imagenReverso}`"
+        @click="showLightbox(1)"
       />
       <div>
-        <fwb-img
+        <Image
           v-if="data?.imagenFoto"
-          class="rounded-xl"
-          alt="flowbite-vue"
+          class="cursor-pointer rounded-xl transition-transform duration-300 ease-in-out hover:scale-105"
           size="w-24 h-24"
           :src="`data:image/jpeg;base64,${data?.imagenFoto}`"
+          @click="showLightbox(2)"
         />
       </div>
+      <VueEasyLightbox
+        :visible="visible"
+        :imgs="imgs"
+        :index="index"
+        @hide="visible = false"
+      />
+    </div>
+    <div
+      v-else
+      class="text-sm text-slate-500"
+    >
+      No hay imagenes para mostrar.
     </div>
   </div>
 </template>
 
 <script setup>
-import { FwbImg } from 'flowbite-vue'
-defineProps(['data', 'loading'])
+import { computed, ref } from 'vue'
+import Image from '@/ui/images/Image.vue'
+import VueEasyLightbox from 'vue-easy-lightbox'
+
+const props = defineProps(['data', 'loading'])
+
+const visible = ref(false)
+const index = ref(0)
+
+const imgs = computed(() => {
+  const { data } = props
+  return [
+    data?.imagenFrontal ? `data:image/jpeg;base64,${data.imagenFrontal}` : null,
+    data?.imagenReverso ? `data:image/jpeg;base64,${data.imagenReverso}` : null,
+    data?.imagenFoto ? `data:image/jpeg;base64,${data.imagenFoto}` : null
+  ].filter(Boolean)
+})
+
+const showLightbox = (i) => {
+  index.value = i
+  visible.value = true
+}
 </script>
---
+++ src/ui/images/Image.vue
@@ -0,0 +1,11 @@
+<template>
+  <fwb-img
+    class="cursor-pointer rounded-xl transition-transform duration-300 ease-in-out hover:scale-105"
+  />
+</template>
+
+<script setup>
+import { FwbImg } from 'flowbite-vue'
+
+// defineProps(['src', 'size'])
+</script>
